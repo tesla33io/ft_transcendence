ff0fb50e69d3adc4dfd08aa3b654af5e887d7c62
eschencode <linuseschenbach@gmail.com>
2025-10-09 12:23:36 +0200

    feat(404): added custom 404 site

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -14,6 +14,7 @@ import {localGameView} from './views/localGamePage'
 import {localGameSetupView} from './views/localGameSetupPage'
 import {remoteGameSetupView} from './views/remoteGameSetup'
 import {settingsView} from './views/settingsPage'
+import { notFoundView } from './views/notFoundPage'
 
 document.addEventListener("DOMContentLoaded", () => {
 	const router = new Router("app");
@@ -31,6 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	router.register("/settings", () => settingsView(router));
 	router.register("/localgame", () => localGameSetupView(router));
 	router.register("/localgame/play", () => localGameView(router));
+	router.register("/404", () => notFoundView(router));
 
 
 
