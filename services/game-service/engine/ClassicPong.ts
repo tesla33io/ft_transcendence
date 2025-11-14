b1ef4e2db2c3cd5fbe839124a31260cdbef492b5
Inzagini <93729938+Inzagini@users.noreply.github.com>
2025-11-14 14:56:54 +0100

    Fix: game engine (#98)
    * fix: change in the ball speed increment
    *fix: change the ball max speed
    *refract: change the goal edge
    
    * fix: engine order of the logic of ball checking and scoring
    
    * refract: put the score litmit to the .env
    
    * improvement in collison checking
    
    * add: improvmenmt on AI logic
    
    * fix: corrected paddle render side

--- services/game-service/engine/ClassicPong.ts
+++ services/game-service/engine/ClassicPong.ts
@@ -1,9 +1,11 @@
 import { GameEngine } from "./GameEngine";
-import { GAME_HEIGHT, GAME_WIDTH, PADDLE_HEIGHT, PADDLE_WIDTH, PLAYER_OFFSET } from "../types/types";
+import { GAME_HEIGHT, GAME_WIDTH, PADDLE_HEIGHT, PADDLE_WIDTH, PLAYER_OFFSET, GAME_SCORE } from "../types/types";
 import { Game, Player} from "../types/interfaces"
 
 export class ClassicPong extends GameEngine {
-	private maxBallSpeed = 18
+	private readonly BALL_MAX_SPEED = 16
+	private readonly K = 2
+	private readonly GOAL_EDGE = 10
 
 	public initializeGameState(game: Game){
 		game.player1.Y = GAME_HEIGHT / 2
@@ -26,16 +28,17 @@ export class ClassicPong extends GameEngine {
 		game.ball.x =  GAME_WIDTH / 2
 		game.ball.y = Math.random() * GAME_HEIGHT
 		game.ball.vx = Math.random() > 0.5 ? 2 : -2
-		game.ball.vy = (Math.random() * 0.5) ? 2 : -2
+		game.ball.vy = (Math.random() > 0.5) ? 2 : -2
 	}
 
 	protected updateGame(game: Game){
 		this.updateBallPosition(game)
 		this.updatePlayerScore(game)
+		this.paddleCollisionCheck(game)
 		if (this.onGameStatusUpdate)
 			this.onGameStatusUpdate(game)
-		if (game.player1.score >= this.GAME_SCORE ||
-			game.player2.score >= this.GAME_SCORE){
+		if (game.player1.score >= GAME_SCORE ||
+			game.player2.score >= GAME_SCORE){
 			this.stopGame(game.id)
 			const winner = game.player1.score > game.player2.score ? game.player1.id : game.player2.id
 			if (this.declareWinner)
@@ -44,12 +47,14 @@ export class ClassicPong extends GameEngine {
 	}
 
 	private updatePlayerScore(game: Game){
-		if (game.ball.x < PLAYER_OFFSET - 10 && game.ball.vx < 0){
+		if (game.ball.x < PLAYER_OFFSET - PADDLE_WIDTH &&
+				game.ball.vx < 0){
 			game.player2.score++
 			this.ballReset(game)
 			console.log(`Player 2 (${game.player2.name}) score!`)
 		}
-		else if (game.ball.x > GAME_WIDTH - PLAYER_OFFSET + 10 && game.ball.vx > 0){
+		else if (game.ball.x > GAME_WIDTH - PLAYER_OFFSET + PADDLE_WIDTH &&
+				game.ball.vx > 0){
 			game.player1.score++
 			this.ballReset(game)
 			console.log(`Player 1 (${game.player1.name}) score!`)
@@ -84,39 +89,40 @@ export class ClassicPong extends GameEngine {
 	}
 
 	protected paddleCollisionCheck(game: Game): void {
-		const K = 4
-		if (game.ball.x <= game.player1.X + PADDLE_WIDTH){
+		if (game.ball.x < game.player1.X + PADDLE_WIDTH && game.ball.vx < 0){
 			this.paddleCollision(game, game.player1)
 		}
 
-		else if (game.ball.x >= game.player2.X - PADDLE_WIDTH) {
+		else if (game.ball.x > game.player2.X - PADDLE_WIDTH && game.ball.vx > 0) {
 			this.paddleCollision(game, game.player2)
 		}
 	}
 
 	private paddleCollision(game: Game, player: Player){
-		const K = 4
 		if ((game.ball.y >= player.Y - 2 &&
 			game.ball.y <= player.Y + 2 )){
 			game.ball.vx = game.ball.vx < 0 ? -2 : 2
 			game.ball.vx *= -1
+			// console.log(`ball colided with paddle player ${player.name} ball: ${game.ball.x} paddle ${game.player1.X} `)
 		}
 
-		else if ((game.ball.y === player.Y - PADDLE_HEIGHT / 2 - K ||
-			game.ball.y === player.Y + PADDLE_HEIGHT / 2 + K)){
-			game.ball.vx = game.ball.vx < 0 ? game.ball.vx - K : game.ball.vx + K
-			if (Math.abs(game.ball.vx) > this.maxBallSpeed)
-				game.ball.vx = game.ball.vx < 0 ? -20 : 20
+		else if ((game.ball.y === player.Y - PADDLE_HEIGHT / 2 - this.K ||
+			game.ball.y === player.Y + PADDLE_HEIGHT / 2 + this.K)){
+			game.ball.vx = game.ball.vx < 0 ? game.ball.vx - this.K : game.ball.vx + this.K
+			if (Math.abs(game.ball.vx) > this.BALL_MAX_SPEED)
+				game.ball.vx = game.ball.vx < 0 ? -this.BALL_MAX_SPEED : this.BALL_MAX_SPEED
 			game.ball.vx *= -1
 			game.ball.vy *= -1
+			// console.log(`ball colided with paddle player ${player.name} ball: ${game.ball.x} paddle ${game.player1.X} `)
 		}
 
 		else if ((game.ball.y >= player.Y - PADDLE_HEIGHT / 2 &&
 			game.ball.y <= player.Y + PADDLE_HEIGHT / 2)){
-			game.ball.vx = game.ball.vx < 0 ? game.ball.vx - K : game.ball.vx + K
-			if (Math.abs(game.ball.vx) > this.maxBallSpeed)
-				game.ball.vx = game.ball.vx < 0 ? -20 : 20
+			game.ball.vx = game.ball.vx < 0 ? game.ball.vx - this.K : game.ball.vx + this.K
+			if (Math.abs(game.ball.vx) > this.BALL_MAX_SPEED)
+				game.ball.vx = game.ball.vx < 0 ? -this.BALL_MAX_SPEED : this.BALL_MAX_SPEED
 			game.ball.vx *= -1
+			// console.log(`ball colided with paddle player ${player.name} ball: ${game.ball.x} paddle ${game.player1.X} `)
 		}
 	}
 }
--- services/game-service/engine/GameEngine.ts
+++ services/game-service/engine/GameEngine.ts
@@ -5,7 +5,6 @@ export abstract class GameEngine {
 	protected activeGames: Map<string, Game> = new Map()
 	protected gameLoops: Map<string, NodeJS.Timeout> = new Map()
 	protected FRAME_TIME = 1000 / FPS
-	protected GAME_SCORE = 3 //set lower for testing
 
 	constructor(){
 		console.log('GameEngine initialized')
@@ -43,7 +42,6 @@ export abstract class GameEngine {
 		game.ball.x += game.ball.vx
 		game.ball.y += game.ball.vy
 
-		this.paddleCollisionCheck(game)
 		if (game.ball.y <= 0){
 			game.ball.y = 10
 			game.ball.vy *= -1
